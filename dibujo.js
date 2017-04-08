ebef00e5905a24b932d24d805ef521381c710f69
Héctor Amparán <hctramprn@gmail.com>
2017-04-07 23:41:29 -0500

    Dibujo con funciones
    Usa una función para dibujar 2 línas

--- dibujo.js
+++ dibujo.js
@@ -1,9 +1,14 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 
-lienzo.beginPath();
-lienzo.strokeStyle = "red";
-lienzo.moveTo(100, 100);
-lienzo.lineTo(200, 200);
-lienzo.stroke();
-lienzo.closePath();
+function dibujarlinea(color, xinicial, yinicial, xfinal, yfinal) {
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
+}
+
+dibujarlinea("pink", 10, 300, 220, 10);
+dibujarlinea("blue", 30, 50, 40, 250);
