ab34dadf9bb70809d242d13a91018820168eab4a
Héctor Amparán <hctramprn@gmail.com>
2017-04-08 00:10:18 -0500

    Curva en canvas 30 lineas
    Dibuja una curva en canvas a partir de 3 líneas rectas

--- dibujo.js
+++ dibujo.js
@@ -1,6 +1,24 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 
+var lineas = 30;
+var espacio = d.width/lineas;
+var xi = 1;
+var yi = 1;
+var xf = 1;
+var yf = 299;
+var color = "blue";
+
+dibujarlinea("blue", 1, 1, 1, 299);
+dibujarlinea("blue", 1, 299, 299, 299);
+
+
+while (xf < d.width) {
+  dibujarlinea(color, xi, yi, xf, yf);
+  yi = yi + espacio;
+  xf = xf + espacio;
+}
+
 function dibujarlinea(color, xinicial, yinicial, xfinal, yfinal) {
   lienzo.beginPath();
   lienzo.strokeStyle = color;
@@ -9,6 +27,3 @@ function dibujarlinea(color, xinicial, yinicial, xfinal, yfinal) {
   lienzo.stroke();
   lienzo.closePath();
 }
-
-dibujarlinea("pink", 10, 300, 220, 10);
-dibujarlinea("blue", 30, 50, 40, 250);
