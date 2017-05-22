1dea2c1056bb939b5e197eb88388b23430951738
Hector Amparan <hctramprn@users.noreply.github.com>
2017-05-22 00:03:28 -0500

    Se agregó la función de elegir cuántas líneas se desean

--- dibujo.js
+++ dibujo.js
@@ -1,6 +1,10 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 
+var texto = document.getElementById("numLineas");
+var boton = document.getElementById("aDarle");
+boton.addEventListener("click", dibujoPorClick);
+
 function dibujarlinea(color, xinicial, yinicial, xfinal, yfinal) {
   lienzo.beginPath();
   lienzo.strokeStyle = color;
@@ -10,5 +14,23 @@ function dibujarlinea(color, xinicial, yinicial, xfinal, yfinal) {
   lienzo.closePath();
 }
 
-dibujarlinea("pink", 10, 300, 220, 10);
-dibujarlinea("blue", 30, 50, 40, 250);
+function dibujoPorClick() {
+  var lineas = parseInt(texto.value);
+  var espacio = d.width/lineas;
+  var xi = 1;
+  var yi = 1;
+  var xf = 1;
+  var yf = 299;
+  var color = "blue";
+
+  dibujarlinea("blue", 1, 1, 1, 299);
+  dibujarlinea("blue", 1, 299, 299, 299);
+
+
+  while (xf < d.width) {
+    dibujarlinea(color, xi, yi, xf, yf);
+    yi = yi + espacio;
+    xf = xf + espacio;
+  }
+
+}
